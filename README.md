f149fd696fe8c96433cbbe559f6cefd9fa5f791b
claude-bot-mh <claude-bot-mh@gil.gmbh>
2026-09-03 13:26:56 +0200

    chore: release 0.8.2-dev

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ damage highscores.
 
 ## Current state
 
-- Addon version: `0.8.1`
+- Addon version: `0.8.2-dev`
 - Current development target: WoW Classic Era / Season of Discovery (`11509`)
 - Titan Panel baseline: unified Titan Panel `9.3.2`
 - Required dependency: Titan Panel (`Titan`)
--- TitanCritLine.lua
+++ TitanCritLine.lua
@@ -3,7 +3,7 @@ DEBUG = false; -- for internal testing only, leave it set to false!
 
 --[[ global addon variables ]]
 local TITAN_CRITLINE_ID =  "CritLine";
-local TITAN_CRITLINE_VERSION = "0.8.1";
+local TITAN_CRITLINE_VERSION = "0.8.2-dev";
 local TITAN_CRITLINE_BUTTON_LABEL = "CL: ";
 local TITAN_CRITLINE_BUTTON_ICON = "Interface\\AddOns\\TitanCritLine\\TitanCritLine";
 local TITAN_CRITLINE_BUTTON_TEXT = "%s/%s/%s";
