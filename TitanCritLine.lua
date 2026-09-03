02b2cff84dc7ba99171ae2fc0f84ec2935b18a10
claude-bot-mh <claude-bot-mh@gil.gmbh>
2026-09-03 18:36:08 +0200

    chore: release 0.8.8-dev

--- TitanCritLine.lua
+++ TitanCritLine.lua
@@ -3,7 +3,7 @@ DEBUG = false; -- for internal testing only, leave it set to false!
 
 --[[ global addon variables ]]
 local TITAN_CRITLINE_ID =  "CritLine";
-local TITAN_CRITLINE_VERSION = "0.8.7-dev";
+local TITAN_CRITLINE_VERSION = "0.8.8-dev";
 local TITAN_CRITLINE_BUTTON_LABEL = "CL: ";
 local TITAN_CRITLINE_BUTTON_ICON = "Interface\\AddOns\\TitanCritLine\\TitanCritLine";
 local TITAN_CRITLINE_BUTTON_TEXT = "%s/%s/%s";
